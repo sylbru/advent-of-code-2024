5faf061f2a63f7017a8183716642edcca0ef2605
sylbru <sylvain.brunerie@gmail.com>
2024-12-02 00:14:50 +0100

    extract run function

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,16 +1,15 @@
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    if let Some((mut list_a, mut list_b)) = parse_one(input) {
-        list_a.sort();
-        list_b.sort();
-        let pairs = list_a.iter().zip(list_b);
-        let result = pairs.fold(0, |acc, (a, b)| acc + ((a - b).abs() as u32));
+    parse_one(input).map(run_one)
+}
 
-        Some(result)
-    } else {
-        None
-    }
+fn run_one((mut list_a, mut list_b): (Vec<i32>, Vec<i32>)) -> u32 {
+    list_a.sort();
+    list_b.sort();
+    let pairs = list_a.iter().zip(list_b);
+    let result = pairs.fold(0, |acc, (a, b)| acc + ((a - b).abs() as u32));
+    result
 }
 
 fn parse_one(input: &str) -> Option<(Vec<i32>, Vec<i32>)> {

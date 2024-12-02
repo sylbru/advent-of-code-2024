a4de13ef0e6cbb2ba3759e5abf939e9afb6e9e2d
sylbru <sylvain.brunerie@gmail.com>
2024-12-02 10:37:11 +0100

    solve 02-2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -8,6 +8,10 @@ fn run_one(reports: Vec<Vec<u32>>) -> u32 {
     reports.iter().filter(is_safe).count() as u32
 }
 
+fn run_two(reports: Vec<Vec<u32>>) -> u32 {
+    reports.iter().filter(is_kinda_safe).count() as u32
+}
+
 enum Direction {
     Decreasing,
     Increasing,
@@ -41,6 +45,27 @@ fn is_safe(report: &&Vec<u32>) -> bool {
     all_gradual && (all_increasing || all_decreasing)
 }
 
+fn is_kinda_safe(report: &&Vec<u32>) -> bool {
+    let simplified_reports = generate_simplified_reports(report);
+    simplified_reports.iter().any(|report| is_safe(&&report))
+}
+
+fn generate_simplified_reports(report: &&Vec<u32>) -> Vec<Vec<u32>> {
+    let mut simplified_reports = Vec::new();
+
+    for index in 0..report.len() {
+        let simplified_report = report
+            .iter()
+            .enumerate()
+            .filter(|&(i, _)| i != index)
+            .map(|(_, &value)| value)
+            .collect();
+        simplified_reports.push(simplified_report);
+    }
+
+    simplified_reports
+}
+
 fn window_to_delta((first, second): (u32, u32)) -> (Direction, u32) {
     let direction = direction_from_adjacent_levels(first, second);
     let difference = first.abs_diff(second);
@@ -61,7 +86,7 @@ fn parse(input: &str) -> Option<Vec<Vec<u32>>> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    parse(input).map(run_two)
 }
 
 #[cfg(test)]
@@ -77,6 +102,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4));
     }
 }

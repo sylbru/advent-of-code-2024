0761d949726b0c779245eef809b2d2f26bce3894
sylbru <sylvain.brunerie@gmail.com>
2024-12-02 08:28:11 +0100

    scaffold day 2

---
+++ src/bin/02.rs
@@ -0,0 +1,38 @@
+advent_of_code::solution!(2);
+
+pub fn part_one(input: &str) -> Option<u32> {
+    parse(input).map(run_one)
+}
+
+fn run_one(reports: Vec<Vec<u32>>) -> u32 {
+    reports.iter().filter(is_safe).count() as u32
+}
+
+fn is_safe(report: &&Vec<u32>) -> bool {
+    true
+}
+
+fn parse(input: &str) -> Option<Vec<Vec<u32>>> {
+    Some(vec![vec![42]])
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(2));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}

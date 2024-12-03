6100e5192e7a77686087536dc0fd7d1944665996
sylbru <sylvain.brunerie@gmail.com>
2024-12-03 09:12:46 +0100

    prepare for part two by parsing to enum

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,3 +1,5 @@
+use std::default;
+
 use regex::Regex;
 
 advent_of_code::solution!(3);
@@ -6,19 +8,31 @@ pub fn part_one(input: &str) -> Option<u32> {
     parse(input).map(run_one)
 }
 
-fn parse(input: &str) -> Option<Vec<(u32, u32)>> {
+enum Instruction {
+    Multiply(u32, u32),
+    Enable,
+    Disable,
+}
+
+fn parse(input: &str) -> Option<Vec<Instruction>> {
     let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
-    let mut instructions: Vec<(u32, u32)> = Vec::new();
+    let mut instructions: Vec<Instruction> = Vec::new();
 
     for (_, [a, b]) in re.captures_iter(input).map(|c| c.extract()) {
-        instructions.push((a.parse::<u32>().unwrap(), b.parse::<u32>().unwrap()));
+        instructions.push(Instruction::Multiply(
+            a.parse::<u32>().unwrap(),
+            b.parse::<u32>().unwrap(),
+        ));
     }
 
     Some(instructions)
 }
 
-fn run_one(pairs: Vec<(u32, u32)>) -> u32 {
-    pairs.iter().fold(0, |acc, &(a, b)| acc + a * b)
+fn run_one(pairs: Vec<Instruction>) -> u32 {
+    pairs.iter().fold(0, |acc, instruction| match instruction {
+        Instruction::Multiply(a, b) => acc + a * b,
+        default => unimplemented!("Enable/Disable"),
+    })
 }
 
 pub fn part_two(input: &str) -> Option<u32> {

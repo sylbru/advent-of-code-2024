178540bc7d394e713552be58415a41c3c504529b
sylbru <sylvain.brunerie@gmail.com>
2024-12-05 07:22:50 +0100

    find and sum middle page numbers

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,7 +1,23 @@
 advent_of_code::solution!(5);
 
-pub fn part_one(input: &str) -> Option<u32> {
-    None
+pub fn part_one(input: &str) -> Option<u8> {
+    let ordered: Vec<Vec<u8>> = vec![
+        vec![75, 47, 61, 53, 29],
+        vec![97, 61, 53, 29, 13],
+        vec![75, 29, 13],
+    ];
+
+    Some(
+        ordered
+            .iter()
+            .map(|update| middle_page_number(update))
+            .fold(0, |acc, page_number| acc + page_number),
+    )
+}
+
+fn middle_page_number(update: &Vec<u8>) -> u8 {
+    let middle_index = (update.len() - 1) / 2;
+    update[middle_index]
 }
 
 pub fn part_two(_input: &str) -> Option<u32> {

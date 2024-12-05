9cbf15f7694bf3e3361237b146b12f581414e02f
sylbru <sylvain.brunerie@gmail.com>
2024-12-05 01:00:35 +0100

    remove unused arguments

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -13,28 +13,28 @@ pub fn part_one(input: &str) -> Option<u32> {
     for y in 0..height {
         for x in 0..width {
             if grid[y][x] == 'X' {
-                if check_horizontal(&grid, x, y, width, height) {
+                if check_horizontal(&grid, x, y, width) {
                     count += 1;
                 }
-                if check_horizontal_backwards(&grid, x, y, width, height) {
+                if check_horizontal_backwards(&grid, x, y) {
                     count += 1;
                 }
-                if check_vertical(&grid, x, y, width, height) {
+                if check_vertical(&grid, x, y, width) {
                     count += 1;
                 }
-                if check_vertical_backwards(&grid, x, y, width, height) {
+                if check_vertical_backwards(&grid, x, y) {
                     count += 1;
                 }
                 if check_diagonal_nw_se(&grid, x, y, width, height) {
                     count += 1;
                 }
-                if check_diagonal_nw_se_backwards(&grid, x, y, width, height) {
+                if check_diagonal_nw_se_backwards(&grid, x, y) {
                     count += 1;
                 }
-                if check_diagonal_sw_ne(&grid, x, y, width, height) {
+                if check_diagonal_sw_ne(&grid, x, y, width) {
                     count += 1;
                 }
-                if check_diagonal_sw_ne_backwards(&grid, x, y, width, height) {
+                if check_diagonal_sw_ne_backwards(&grid, x, y, width) {
                     count += 1;
                 }
             }
@@ -44,37 +44,19 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(count)
 }
 
-fn check_horizontal(
-    grid: &Vec<Vec<char>>,
-    x: usize,
-    y: usize,
-    width: usize,
-    height: usize,
-) -> bool {
+fn check_horizontal(grid: &Vec<Vec<char>>, x: usize, y: usize, width: usize) -> bool {
     x <= width - 4 && grid[y][x + 1] == 'M' && grid[y][x + 2] == 'A' && grid[y][x + 3] == 'S'
 }
 
-fn check_horizontal_backwards(
-    grid: &Vec<Vec<char>>,
-    x: usize,
-    y: usize,
-    width: usize,
-    height: usize,
-) -> bool {
+fn check_horizontal_backwards(grid: &Vec<Vec<char>>, x: usize, y: usize) -> bool {
     x >= 3 && grid[y][x - 1] == 'M' && grid[y][x - 2] == 'A' && grid[y][x - 3] == 'S'
 }
 
-fn check_vertical(grid: &Vec<Vec<char>>, x: usize, y: usize, width: usize, height: usize) -> bool {
+fn check_vertical(grid: &Vec<Vec<char>>, x: usize, y: usize, height: usize) -> bool {
     y <= height - 4 && grid[y + 1][x] == 'M' && grid[y + 2][x] == 'A' && grid[y + 3][x] == 'S'
 }
 
-fn check_vertical_backwards(
-    grid: &Vec<Vec<char>>,
-    x: usize,
-    y: usize,
-    width: usize,
-    height: usize,
-) -> bool {
+fn check_vertical_backwards(grid: &Vec<Vec<char>>, x: usize, y: usize) -> bool {
     y >= 3 && grid[y - 1][x] == 'M' && grid[y - 2][x] == 'A' && grid[y - 3][x] == 'S'
 }
 
@@ -89,24 +71,12 @@ fn check_diagonal_nw_se(
         && (grid[y + 1][x + 1] == 'M' && grid[y + 2][x + 2] == 'A' && grid[y + 3][x + 3] == 'S')
 }
 
-fn check_diagonal_nw_se_backwards(
-    grid: &Vec<Vec<char>>,
-    x: usize,
-    y: usize,
-    width: usize,
-    height: usize,
-) -> bool {
+fn check_diagonal_nw_se_backwards(grid: &Vec<Vec<char>>, x: usize, y: usize) -> bool {
     (y >= 3 && x >= 3)
         && (grid[y - 1][x - 1] == 'M' && grid[y - 2][x - 2] == 'A' && grid[y - 3][x - 3] == 'S')
 }
 
-fn check_diagonal_sw_ne(
-    grid: &Vec<Vec<char>>,
-    x: usize,
-    y: usize,
-    width: usize,
-    height: usize,
-) -> bool {
+fn check_diagonal_sw_ne(grid: &Vec<Vec<char>>, x: usize, y: usize, width: usize) -> bool {
     (y >= 3 && x <= width - 4)
         && (grid[y - 1][x + 1] == 'M' && grid[y - 2][x + 2] == 'A' && grid[y - 3][x + 3] == 'S')
 }
@@ -115,7 +85,6 @@ fn check_diagonal_sw_ne_backwards(
     grid: &Vec<Vec<char>>,
     x: usize,
     y: usize,
-    width: usize,
     height: usize,
 ) -> bool {
     (y <= height - 4 && x >= 3)

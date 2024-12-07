fe2647a21184cef678ce1d1d78f62c06da0cb33a
sylbru <sylvain.brunerie@gmail.com>
2024-12-07 10:26:15 +0100

    parsing ok

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,7 +1,30 @@
 advent_of_code::solution!(7);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    println!("{:?}", parse(input));
+    parse(input).map(run_one)
+}
+
+fn run_one(tests: Vec<(u32, Vec<u32>)>) -> u32 {
+    42
+}
+
+fn parse(input: &str) -> Option<Vec<(u32, Vec<u32>)>> {
+    Some(
+        input
+            .lines()
+            .map(|line| line.split_once(":").unwrap())
+            .map(|(result, operands_str)| {
+                (
+                    result.parse().unwrap(),
+                    operands_str
+                        .split(" ")
+                        .filter_map(|operand| operand.parse().ok())
+                        .collect(),
+                )
+            })
+            .collect(),
+    )
 }
 
 pub fn part_two(input: &str) -> Option<u32> {

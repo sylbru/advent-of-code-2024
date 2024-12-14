bdc5ca2e52f080f029d68be2b603bbeefc0bb35c
sylbru <sylvain.brunerie@gmail.com>
2024-12-15 00:12:53 +0100

    brute force

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -11,7 +11,40 @@ fn parse(input: &str) -> Option<Vec<usize>> {
 
 pub fn part_one(input: &str) -> Option<u32> {
     let stones = parse(input).unwrap();
-    None
+
+    Some(blink(stones, 25))
+}
+
+fn transform_stone(stone: usize) -> Vec<usize> {
+    match stone {
+        0 => vec![1],
+        x => {
+            let x_str = x.to_string();
+            let x_str_len = x_str.len();
+            if x_str_len % 2 == 0 {
+                // split
+                let (a_str, b_str) = x_str.split_at(x_str_len / 2);
+                vec![a_str.parse().unwrap(), b_str.parse().unwrap()]
+            } else {
+                // multiply
+                vec![x * 2024]
+            }
+        }
+    }
+}
+
+fn blink(stones: Vec<usize>, times: u8) -> u32 {
+    if (times > 0) {
+        let new_stones: Vec<usize> = stones
+            .iter()
+            .map(|&stone| transform_stone(stone))
+            .collect::<Vec<Vec<usize>>>()
+            .concat();
+        blink(new_stones, times - 1)
+    } else {
+        println!("{:?}", stones);
+        stones.len() as u32
+    }
 }
 
 pub fn part_two(input: &str) -> Option<u32> {

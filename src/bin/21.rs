dacb88565ab10917b4f00315e680e2e134e86329
sylbru <sylvain.brunerie@gmail.com>
2024-12-23 01:14:38 +0100

    refactor and start next step

--- src/bin/21.rs
+++ src/bin/21.rs
@@ -1,3 +1,5 @@
+#![allow(unused)]
+
 advent_of_code::solution!(21);
 
 pub fn part_one(input: &str) -> Option<u32> {
@@ -15,48 +17,75 @@ fn numpad_to_dirpad(code: &str) -> String {
     let mut sequence = "".to_owned();
 
     for target in code.chars() {
-        // move to char, then (A)ctivate
-        while current != target {
-            // move in the right direction
-            let dy: i8 = row_for_char(target) - y;
-            let dx: i8 = col_for_char(target) - x;
+        // move to char
+        let dy: i8 = row_for_char(target) - y;
+        let dx: i8 = col_for_char(target) - x;
 
-            if dx < 0 && dy < 0 {
-                // going NW, avoid hole
-                // move vertically then horizontally
-                let count_vert = (-dy) as usize;
-                sequence.push_str(&"^".repeat(count_vert));
+        if dx < 0 && dy < 0 {
+            // going NW, avoid hole
+            // move vertically then horizontally
+            let count_vert = (-dy) as usize;
+            sequence.push_str(&"^".repeat(count_vert));
 
-                let count_horiz = (-dx) as usize;
+            let count_horiz = (-dx) as usize;
+            sequence.push_str(&"<".repeat(count_horiz));
+        } else {
+            // general case
+            // move horizontally then vertically
+            let count_horiz = dx.abs() as usize;
+            if (dx < 0) {
                 sequence.push_str(&"<".repeat(count_horiz));
             } else {
-                // move horizontally then vertically
-                let count_horiz = dx.abs() as usize;
-                if (dx < 0) {
-                    sequence.push_str(&"<".repeat(count_horiz));
-                } else {
-                    sequence.push_str(&">".repeat(count_horiz));
-                }
-
-                let count_vert = dy.abs() as usize;
-                if (dy < 0) {
-                    sequence.push_str(&"^".repeat(count_vert));
-                } else {
-                    sequence.push_str(&"v".repeat(count_vert));
-                }
+                sequence.push_str(&">".repeat(count_horiz));
             }
-            println!("{}", sequence);
-            x = x + dx;
-            y = y + dy;
-            current = numpad()[y as usize][x as usize];
-            // update current with new character
+
+            let count_vert = dy.abs() as usize;
+            if (dy < 0) {
+                sequence.push_str(&"^".repeat(count_vert));
+            } else {
+                sequence.push_str(&"v".repeat(count_vert));
+            }
         }
+
+        x = x + dx;
+        y = y + dy;
+        current = numpad()[y as usize][x as usize];
 
         sequence.push('A');
     }
 
     sequence
     // <A^A>^^AvvvA
+}
+
+fn dirpad_to_dirpad(sequence: String) -> String {
+    let mut current = 'A';
+    let mut y = row_for_char_dirpad(&current);
+    let mut x = col_for_char_dirpad(&current);
+    println!("{} {}", y, x);
+    let mut sequence = "".to_owned();
+    "aiue".to_owned()
+}
+
+fn dirpad() -> Vec<Vec<char>> {
+    vec![vec!['⌧', '^', 'A'], vec!['<', 'v', '>']]
+}
+
+fn row_for_char_dirpad(current: &char) -> usize {
+    dirpad()
+        .iter()
+        .position(|row| row.contains(current))
+        .unwrap()
+}
+
+fn col_for_char_dirpad(current: &char) -> usize {
+    dirpad()
+        .iter()
+        .find(|row| row.contains(current))
+        .unwrap()
+        .iter()
+        .position(|character| character == current)
+        .unwrap()
 }
 
 fn numpad() -> Vec<Vec<char>> {
@@ -123,13 +152,11 @@ mod tests {
         assert_eq!(numpad_to_dirpad("0"), "<A");
     }
 
-    #[ignore]
     #[test]
     fn test_numpad_0A() {
         assert_eq!(numpad_to_dirpad("0A"), "<A>A");
     }
 
-    #[ignore]
     #[test]
     fn test_numpad_02A() {
         assert_eq!(numpad_to_dirpad("02A"), "<A^Av>A");
@@ -138,6 +165,14 @@ mod tests {
     #[test]
     fn test_numpad_to_dirpad() {
         assert_eq!(numpad_to_dirpad("029A"), "<A^A>^^AvvvA");
+    }
+
+    #[test]
+    fn test_dirpad_to_dirpad() {
+        assert_eq!(
+            dirpad_to_dirpad("<A^A>^^AvvvA".to_owned()),
+            "v<<A>>^A<A>AvA<^AA>A<vAAA>^A"
+        );
     }
 
     #[ignore]

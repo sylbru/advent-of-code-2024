bda510bf6376ef7210cf86915d225504d8903268
sylbru <sylvain.brunerie@gmail.com>
2024-12-23 08:22:15 +0100

    calculate 2000th secret number

--- src/bin/22.rs
+++ src/bin/22.rs
@@ -10,11 +10,20 @@ pub fn part_two(input: &str) -> Option<u32> {
     None
 }
 
-fn next(secret_number: usize) -> usize {
+fn calculate_2000th_secret_number(initial: usize) -> usize {
+    let mut next = initial;
+
+    for i in 0..2000 {
+        next = next_secret_number(next);
+    }
+
+    next
+}
+
+fn next_secret_number(secret_number: usize) -> usize {
     let step_1 = prune(mix(secret_number, secret_number * 64));
     let step_2 = prune(mix(step_1, step_1 / 32));
     let step_3 = prune(mix(step_2, step_2 * 2048));
-    println!("{} {} {} {} ", secret_number, step_1, step_2, step_3,);
     step_3
 }
 
@@ -42,8 +51,14 @@ mod tests {
 
     #[test]
     fn test_next() {
-        assert_eq!(next(123), 15887950);
-        assert_eq!(next(16495136), 527345);
+        assert_eq!(next_secret_number(123), 15887950);
+        assert_eq!(next_secret_number(16495136), 527345);
+    }
+
+    #[test]
+    fn test_2000th_secret_number() {
+        assert_eq!(calculate_2000th_secret_number(1), 8685429);
+        assert_eq!(calculate_2000th_secret_number(2024), 8667524);
     }
 
     #[test]
